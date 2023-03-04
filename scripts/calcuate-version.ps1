c3e0d95368be796a51c7dd65ea7b93aedd60c3d4
Puneet Punj <puneet.punj@iress.com>
2023-03-04 17:55:57 +1100

    create git tag only in pipeline

--- scripts/calcuate-version.ps1
+++ scripts/calcuate-version.ps1
@@ -16,3 +16,10 @@ Write-Output $gitVersionMetadata
 Write-Host "`n--- Calculated Versions"
 $calulatedVersions = Get-CalculatedVersions $gitVersionMetadata
 Write-Output $calulatedVersions | ConvertTo-Json | ConvertFrom-Json
+
+# create git tag only in CI pipeline
+if ($env:CI){
+    $message = "Build: $env:BUILD_URL"
+    git tag "v${$calulatedVersions["Version"]}" -m "${message}"
+    git push --tags
+}
